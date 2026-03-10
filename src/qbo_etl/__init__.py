a9b893205b45eaf1f41f8c7cf4d17ce7ffc3ad2e
ZheRao <zheraoeminem@gmail.com>
2026-03-09 21:20:04 -0600

    clean up

--- src/qbo_etl/__init__.py
+++
@@ -1,7 +0,0 @@
-from qbo_etl.io.file_read import read_configs
-from qbo_etl.utils.task_schedular import flatten_job_scheduler
-
-__all__={
-    "read_configs",
-    "flatten_job_scheduler"
-}
--- src/qbo_etl/io/file_read.py
+++
@@ -1,9 +0,0 @@
-from importlib.resources import files
-import json
-
-def read_configs(config_type:str, name:str) -> dict:
-    """
-    Reads and return configurations stored in json_configs/config_type/name, e.g., json_configs/io/path.win.json
-    """
-    p = files("qbo_etl.json_configs").joinpath(f"{config_type}/{name}")
-    return json.loads(p.read_text())
